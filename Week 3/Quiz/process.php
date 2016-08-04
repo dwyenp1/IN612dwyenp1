b7feeb63d1b3c68f5ef42642d1e322cb168371b9
dwyenp1 <dwyenp1@student.op.ac.nz>
2016-08-05 00:14:25 +1200

    Just need to add table CSS and strip_tags

--- Week 3/Quiz/process.php
+++ Week 3/Quiz/process.php
@@ -1,5 +1,35 @@
 <?php
-	foreach($_POST as $field=>$value)
-		if($field!="submitted")
-			echo("$field: $value<br>");
+	echo("<table>");
+
+	echo("
+		<tr>
+			<th>Field</th>
+			<th>Value</th>
+		</tr>
+		");
+
+		foreach($_POST as $field=>$value)
+		{
+			if($field != "submitted")
+			{
+				if($field == "city")
+				{
+					//Remember that the city array is within $_POST
+					foreach($_POST["city"] as $_POST["city"]=>$value)
+					{
+						echo("<tr>");
+						echo("<td>$field:</td><td>$value</td>");
+						echo("</tr>");
+					}
+				}
+				else
+				{
+					echo("<tr>");
+					echo("<td>$field:</td><td>$value</td>");
+					echo("</tr>");
+				}
+			}	
+		}
+	
+	echo("</table>");			
 ?>
